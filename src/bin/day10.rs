c7aa1cea06e068be16b4475ec0cc371a12040310
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2024-02-29 19:17:55 +0100

    fix day10 challenge2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -10,11 +10,6 @@ use hashbrown::HashSet;
 // . is ground; there is no pipe in this tile.
 // S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 
-const CONNECTED_FROM_NORTH: [char; 3] = ['|', '7', 'F'];
-const CONNECTED_FROM_EAST: [char; 3] = ['-', 'J', '7'];
-const _CONNECTED_FROM_WEST: [char; 3] = ['-', 'L', 'F']; // Not used yet, might need later
-const CONNECTED_FROM_SOUTH: [char; 3] = ['|', 'L', 'J'];
-
 #[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
 enum Direction {
     North,
@@ -35,6 +30,81 @@ impl Direction {
 }
 
 const DIRECTIONS: [Direction; 4] = [Direction::North, Direction::South, Direction::East, Direction::West];
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq)]
+enum PipeType {
+    TopLeft,
+    TopRight,
+    BotLeft,
+    BotRight,
+    Vertical,
+    Horizontal,
+}
+
+impl PipeType {
+    fn from_char(item: &char) -> Option<PipeType> {
+        match item {
+            &'|' => Some(PipeType::Vertical),
+            &'-' => Some(PipeType::Horizontal),
+            &'L' => Some(PipeType::BotLeft),
+            &'F' => Some(PipeType::TopLeft),
+            &'J' => Some(PipeType::BotRight),
+            &'7' => Some(PipeType::TopRight),
+            _ => None,
+        }
+    }
+
+    fn get_connected_directions(&self) -> [Direction; 2] {
+        match self {
+            PipeType::Vertical => [Direction::North, Direction::South],
+            PipeType::Horizontal => [Direction::East, Direction::West],
+            PipeType::TopLeft => [Direction::South, Direction::East],
+            PipeType::TopRight => [Direction::South, Direction::West],
+            PipeType::BotLeft => [Direction::North, Direction::East],
+            PipeType::BotRight => [Direction::North, Direction::West],
+        }
+    }
+
+    fn get_unconnected_directions(&self) -> [Direction; 2] {
+        match self {
+            PipeType::Horizontal => [Direction::North, Direction::South],
+            PipeType::Vertical => [Direction::East, Direction::West],
+            PipeType::BotRight => [Direction::South, Direction::East],
+            PipeType::BotLeft => [Direction::South, Direction::West],
+            PipeType::TopRight => [Direction::North, Direction::East],
+            PipeType::TopLeft => [Direction::North, Direction::West],
+        }
+    }
+
+    fn connects_to_direction(&self, direction: &Direction) -> bool {
+        self.get_connected_directions().contains(direction)
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            PipeType::Horizontal => '-',
+            PipeType::Vertical => '|',
+            PipeType::BotLeft => 'L',
+            PipeType::BotRight => 'J',
+            PipeType::TopLeft => 'F',
+            PipeType::TopRight => '7',
+        }
+    }
+
+    fn to_pipechar(&self) -> char {
+        // ┌─┐  ╔═╗
+        // │ │  ║ ║
+        // └─┘  ╚═╝
+        match self {
+            PipeType::Horizontal => '─',
+            PipeType::Vertical => '│',
+            PipeType::BotLeft => '└',
+            PipeType::BotRight => '┘',
+            PipeType::TopLeft => '┌',
+            PipeType::TopRight => '┐',
+        }
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 struct Position {
@@ -83,34 +153,33 @@ impl Position {
             Direction::West => Position { col: self.col - 1, ..*self },
         }
     }
-    fn get_positions(&self, pipe: &char) -> [Position; 2] {
-        match *pipe {
-            '|' => [self.get_position(Direction::North), self.get_position(Direction::South)],
-            '-' => [self.get_position(Direction::West), self.get_position(Direction::East)],
-            'L' => [self.get_position(Direction::North), self.get_position(Direction::East)],
-            'J' => [self.get_position(Direction::North), self.get_position(Direction::West)],
-            '7' => [self.get_position(Direction::West), self.get_position(Direction::South)],
-            'F' => [self.get_position(Direction::South), self.get_position(Direction::East)],
-            _ => panic!("called get_positions of non-pipe character: {pipe}"),
+    fn get_positions(&self, pipe: &PipeType) -> [Position; 2] {
+        match pipe {
+            PipeType::Vertical => [self.get_position(Direction::North), self.get_position(Direction::South)],
+            PipeType::Horizontal => [self.get_position(Direction::West), self.get_position(Direction::East)],
+            PipeType::BotLeft => [self.get_position(Direction::North), self.get_position(Direction::East)],
+            PipeType::BotRight => [self.get_position(Direction::North), self.get_position(Direction::West)],
+            PipeType::TopRight => [self.get_position(Direction::West), self.get_position(Direction::South)],
+            PipeType::TopLeft => [self.get_position(Direction::South), self.get_position(Direction::East)],
         }
     }
 }
 
 #[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
-struct VisitedBorder {
+struct PositionFromDirection {
     pos: Position,
     direction: Direction,
 }
 
-impl VisitedBorder {
-    fn get_positions(&self, pipe: &char) -> [VisitedBorder; 2] {
-        match *pipe {
-            '|' | '-' => {
+impl PositionFromDirection {
+    fn get_positions(&self, pipe: &PipeType) -> [PositionFromDirection; 2] {
+        match pipe {
+            PipeType::Vertical | PipeType::Horizontal => {
                 let new_pos = self.pos.get_positions(pipe);
-                [VisitedBorder { pos: new_pos[0], ..*self }, VisitedBorder { pos: new_pos[1], ..*self }]
+                [PositionFromDirection { pos: new_pos[0], ..*self }, PositionFromDirection { pos: new_pos[1], ..*self }]
             }
-            'L' => [
-                VisitedBorder {
+            PipeType::BotLeft => [
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::North),
                     direction: if self.direction == Direction::West || self.direction == Direction::South {
                         Direction::West
@@ -118,7 +187,7 @@ impl VisitedBorder {
                         Direction::East
                     },
                 },
-                VisitedBorder {
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::East),
                     direction: if self.direction == Direction::West || self.direction == Direction::South {
                         Direction::South
@@ -127,8 +196,8 @@ impl VisitedBorder {
                     },
                 },
             ],
-            'J' => [
-                VisitedBorder {
+            PipeType::BotRight => [
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::North),
                     direction: if self.direction == Direction::South || self.direction == Direction::East {
                         Direction::East
@@ -136,7 +205,7 @@ impl VisitedBorder {
                         Direction::West
                     },
                 },
-                VisitedBorder {
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::West),
                     direction: if self.direction == Direction::East || self.direction == Direction::South {
                         Direction::South
@@ -145,8 +214,8 @@ impl VisitedBorder {
                     },
                 },
             ],
-            '7' => [
-                VisitedBorder {
+            PipeType::TopRight => [
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::South),
                     direction: if self.direction == Direction::North || self.direction == Direction::East {
                         Direction::East
@@ -154,7 +223,7 @@ impl VisitedBorder {
                         Direction::West
                     },
                 },
-                VisitedBorder {
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::West),
                     direction: if self.direction == Direction::North || self.direction == Direction::East {
                         Direction::North
@@ -163,8 +232,8 @@ impl VisitedBorder {
                     },
                 },
             ],
-            'F' => [
-                VisitedBorder {
+            PipeType::TopLeft => [
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::South),
                     direction: if self.direction == Direction::North || self.direction == Direction::West {
                         Direction::West
@@ -172,7 +241,7 @@ impl VisitedBorder {
                         Direction::East
                     },
                 },
-                VisitedBorder {
+                PositionFromDirection {
                     pos: self.pos.get_position(Direction::East),
                     direction: if self.direction == Direction::North || self.direction == Direction::West {
                         Direction::North
@@ -181,8 +250,33 @@ impl VisitedBorder {
                     },
                 },
             ],
-            _ => panic!("called get_positions of non-pipe character: {pipe}"),
         }
+    }
+    fn get_in_cluster(&self, pipe: &PipeType, max_pos: &Position) -> Vec<PositionFromDirection> {
+        let mut result: Vec<PositionFromDirection> = vec![];
+        match pipe {
+            PipeType::Vertical | PipeType::Horizontal => {
+                let part_res = self.pos.get_position_in_bounds(self.direction, max_pos);
+                if part_res.is_some() {
+                    result.push(PositionFromDirection { pos: part_res.unwrap(), direction: self.direction.opposite() });
+                }
+            }
+            _ => {
+                let unconnected_directions = pipe.get_unconnected_directions();
+                if unconnected_directions.contains(&self.direction) {
+                    for direction in unconnected_directions {
+                        let part_res = self.pos.get_position_in_bounds(direction, max_pos);
+                        if part_res.is_some() {
+                            result.push(PositionFromDirection {
+                                pos: part_res.unwrap(),
+                                direction: direction.opposite(),
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        result
     }
 }
 
@@ -197,34 +291,37 @@ fn find_start_position(grid: &Vec<Vec<char>>) -> Option<Position> {
     None
 }
 
-fn find_start_pipe(grid: &Vec<Vec<char>>, start: Position) -> char {
-    fn get_pipe_at(grid: &Vec<Vec<char>>, pos: Position) -> char {
-        *grid.get(pos.row).unwrap_or(&vec![]).get(pos.col).unwrap_or(&'.')
-    }
-    let north = if start.row > 0 { get_pipe_at(grid, start.get_position(Direction::North)) } else { '.' };
-    let east = get_pipe_at(grid, start.get_position(Direction::East));
-    let south = get_pipe_at(grid, start.get_position(Direction::South));
-    if CONNECTED_FROM_NORTH.contains(&north) {
-        if CONNECTED_FROM_EAST.contains(&east) {
-            'L'
-        } else if CONNECTED_FROM_SOUTH.contains(&south) {
-            '|'
-        } else {
-            'J'
+fn find_start_pipe(grid: &Vec<Vec<char>>, start: Position, max_pos: &Position) -> PipeType {
+    fn get_pipe_at(grid: &Vec<Vec<char>>, pos: Option<Position>) -> Option<PipeType> {
+        match pos {
+            Some(p) => PipeType::from_char(&grid[p.row][p.col]),
+            None => None,
         }
-    } else if CONNECTED_FROM_EAST.contains(&east) {
-        if CONNECTED_FROM_SOUTH.contains(&south) {
-            'F'
+    }
+    let north = get_pipe_at(grid, start.get_position_in_bounds(Direction::North, max_pos));
+    let east = get_pipe_at(grid, start.get_position_in_bounds(Direction::East, max_pos));
+    let south = get_pipe_at(grid, start.get_position_in_bounds(Direction::South, max_pos));
+    if north.is_some_and(|north| north.connects_to_direction(&Direction::South)) {
+        if east.is_some_and(|east| east.connects_to_direction(&Direction::West)) {
+            PipeType::BotLeft
+        } else if south.is_some_and(|south| south.connects_to_direction(&Direction::North)) {
+            PipeType::Vertical
         } else {
-            '-'
+            PipeType::BotRight
+        }
+    } else if east.is_some_and(|east| east.connects_to_direction(&Direction::West)) {
+        if south.is_some_and(|south| south.connects_to_direction(&Direction::North)) {
+            PipeType::TopLeft
+        } else {
+            PipeType::Horizontal
         }
     } else {
-        '7'
+        PipeType::TopRight
     }
 }
 
 fn get_new_position(grid: &Vec<Vec<char>>, pos: Position, marked_positions: &HashSet<Position>) -> Option<Position> {
-    let positions = pos.get_positions(&grid[pos.row][pos.col]);
+    let positions = pos.get_positions(&PipeType::from_char(&grid[pos.row][pos.col]).expect("Not a pipe"));
     if !marked_positions.contains(&positions[0]) {
         Some(positions[0])
     } else if !marked_positions.contains(&positions[1]) {
@@ -237,7 +334,7 @@ fn get_new_position(grid: &Vec<Vec<char>>, pos: Position, marked_positions: &Has
 fn find_marked_positions(grid: &Vec<Vec<char>>, start: Position, start_pipe: &char) -> HashSet<Position> {
     let mut marked_positions = HashSet::new();
     marked_positions.insert(start);
-    let mut next_positions = start.get_positions(start_pipe);
+    let mut next_positions = start.get_positions(&PipeType::from_char(start_pipe).expect("not a pipe char"));
     loop {
         for i in 0..=1 {
             marked_positions.insert(next_positions[i]);
@@ -250,9 +347,10 @@ fn find_marked_positions(grid: &Vec<Vec<char>>, start: Position, start_pipe: &ch
 }
 
 fn challenge1(grid: &Vec<Vec<char>>) -> usize {
+    let max_pos = Position { row: grid.len() - 1, col: grid[0].len() - 1 };
     let start = find_start_position(grid).expect("Could not find start position");
-    let start_pipe = find_start_pipe(grid, start);
-    let positions = find_marked_positions(grid, start, &start_pipe).len();
+    let start_pipe = find_start_pipe(grid, start, &max_pos);
+    let positions = find_marked_positions(grid, start, &start_pipe.to_char()).len();
     if positions % 2 == 1 {
         (positions - 1) / 2
     } else {
@@ -264,7 +362,7 @@ fn challenge1(grid: &Vec<Vec<char>>) -> usize {
 struct Cluster {
     is_outside: bool,
     positions: HashSet<Position>,
-    border: HashSet<VisitedBorder>,
+    border: HashSet<PositionFromDirection>,
 }
 
 impl Cluster {
@@ -285,29 +383,30 @@ impl Cluster {
                 None => self.is_outside = true,
             }
         }
-        println!("Cluster contains {} items and is outside: {}", self.positions.len(), self.is_outside);
-        println!("try sqeezing now");
         // walk along border
-        let mut cur_borders: Vec<VisitedBorder> =
+        let mut cur_borders: Vec<PositionFromDirection> =
             vec![*self.border.iter().next().expect("a cluster must touch a border")];
         let mut walked_border: HashSet<Position> = HashSet::new();
         while !cur_borders.is_empty() {
-            println!("cur borders: {cur_borders:?}, cluster size: {}", self.positions.len());
             walked_border.extend(cur_borders.iter().map(|border| border.pos));
-            let mut next_borders: Vec<VisitedBorder> = vec![];
+            let mut next_borders: Vec<PositionFromDirection> = vec![];
             for border in cur_borders {
-                let new_borders = border.get_positions(&grid[border.pos.row][border.pos.col]);
+                let new_borders =
+                    border.get_positions(&PipeType::from_char(&grid[border.pos.row][border.pos.col]).unwrap());
                 for new_border in new_borders {
                     if walked_border.contains(&new_border.pos) {
                         continue;
                     }
-                    let potential_cluster_part = new_border.pos.get_position_in_bounds(new_border.direction, max_pos);
-                    if potential_cluster_part.is_some() {
+                    let potential_cluster_part = new_border.get_in_cluster(
+                        &PipeType::from_char(&grid[new_border.pos.row][new_border.pos.col]).unwrap(),
+                        max_pos,
+                    );
+                    for potential_pos_from_direction in potential_cluster_part {
                         self._fill_cluster(
                             path,
-                            potential_cluster_part.unwrap(),
+                            potential_pos_from_direction.pos,
                             max_pos,
-                            new_border.direction.opposite(),
+                            potential_pos_from_direction.direction,
                         );
                     }
                     next_borders.push(new_border);
@@ -315,7 +414,6 @@ impl Cluster {
             }
             cur_borders = next_borders;
         }
-        println!("Cluster contains {} items and is outside: {}", self.positions.len(), self.is_outside);
     }
 
     fn _fill_cluster(&mut self, path: &HashSet<Position>, pos: Position, max_pos: &Position, from: Direction) {
@@ -323,7 +421,7 @@ impl Cluster {
             return;
         }
         if path.contains(&pos) {
-            self.border.insert(VisitedBorder { pos: pos, direction: from });
+            self.border.insert(PositionFromDirection { pos: pos, direction: from });
             return;
         }
 
@@ -354,14 +452,40 @@ fn get_clusters(grid: &Vec<Vec<char>>, path: &HashSet<Position>, max_pos: &Posit
     clusters
 }
 
+fn print_grid(grid: &Vec<Vec<char>>, path: &HashSet<Position>, clusters: &Vec<Cluster>) {
+    let mut inside_pos: HashSet<Position> = HashSet::new();
+    for cluster in clusters {
+        if cluster.is_outside {
+            continue;
+        }
+        for pos in &cluster.positions {
+            inside_pos.insert(*pos);
+        }
+    }
+    for (row_index, row) in grid.iter().enumerate() {
+        for (col_index, item) in row.iter().enumerate() {
+            let pos = Position { row: row_index, col: col_index };
+            if inside_pos.contains(&pos) {
+                print!("i");
+            } else if path.contains(&pos) {
+                print!("{}", PipeType::from_char(item).unwrap().to_pipechar());
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
 fn challenge2(grid: &Vec<Vec<char>>) -> usize {
-    let start = find_start_position(grid).expect("Could not find start position");
-    let start_pipe = find_start_pipe(grid, start);
-    let mut adjusted_grid = grid.clone();
-    adjusted_grid[start.row][start.col] = start_pipe;
-    let path = find_marked_positions(grid, start, &start_pipe);
     let max_pos = Position { row: grid.len() - 1, col: grid[0].len() - 1 };
+    let start = find_start_position(grid).expect("Could not find start position");
+    let start_pipe = find_start_pipe(grid, start, &max_pos);
+    let mut adjusted_grid = grid.clone();
+    adjusted_grid[start.row][start.col] = start_pipe.to_char();
+    let path = find_marked_positions(grid, start, &start_pipe.to_char());
     let clusters = get_clusters(&adjusted_grid, &path, &max_pos);
+    print_grid(&adjusted_grid, &path, &clusters);
     clusters.iter().filter(|cluster| !cluster.is_outside).map(|cluster| cluster.positions.len()).sum()
     // 592 is too high
 }
